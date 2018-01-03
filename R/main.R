9a8df4c4f0fcf6bf9741a27645d2a78617293f94
Navin Chandan <navin.chandan@elucidata.io>
2018-01-03 19:41:27 +0530

    Added parse_gct and fix.datatype functions

--- R/main.R
+++ R/main.R
@@ -19,7 +19,8 @@ setClass("GCT_object",
            cid = "character",
            rdesc = "data.frame",
            cdesc = "data.frame",
-           version = "character"
+           version = "character",
+           src = "character"
          )
 )
 
@@ -50,12 +51,13 @@ setValidity("GCT_object",
 # define the initialization method for the GCT_object class
 setMethod("initialize",
           signature = "GCT_object",
-          definition = function(.Object, mat, rdesc = NULL, cdesc = NULL,
-                                version = c("#1.3", "#1.2")) {
-            .Object@mat = mat
-            .Object@rid = rownames(mat)
-            .Object@cid = colnames(mat)
-            
+          definition = function(.Object, mat = NULL, rdesc = NULL, cdesc = NULL,
+                                version = c("#1.3", "#1.2"), src = NULL) {
+            if (!is.null(mat)) {
+              .Object@mat = mat
+              .Object@rid = rownames(mat)
+              .Object@cid = colnames(mat)
+            }
             if (!is.null(rdesc)) {
               if (nrow(rdesc) != nrow(mat)) {
                 stop("rdesc must have same number of rows as matrix has rows")
@@ -78,15 +80,19 @@ setMethod("initialize",
               .Object@cdesc = data.frame()
             }
             
-            version <- match.arg(version)
-            if (version == "#1.3") {
-              if (is.null(cdesc)) {
-                stop("Column descriptors not provided, provide cdesc or set version to #1.2")
+            if (!is.null(src)) {
+              .Object@src <- src
+            }else {
+              version <- match.arg(version)
+              if (version == "#1.3") {
+                if (is.null(cdesc)) {
+                  stop("Column descriptors not provided, provide cdesc or set version to #1.2")
+                }else{
+                  .Object@version <- version
+                }
               }else{
                 .Object@version <- version
               }
-            }else{
-              .Object@version <- version
             }
             return(.Object)
           }
@@ -95,8 +101,6 @@ setMethod("initialize",
 #' Create a GCT_object for a given numeric matrix
 #' 
 #' @param mat a numeric matrix
-#' @param rid a character vector of row ids
-#' @param cid a character vector of column ids
 #' @param rdesc a \code{data.frame} of row descriptors
 #' @param cdesc a \code{data.frame} of column descriptors
 #' @param version version of the gct
@@ -111,6 +115,7 @@ to_GCT <- function(mat, cdesc=NULL, rdesc=NULL, version = NULL) {
             version = version)
   return(ds)
 }
+
 
 #' Write a GCT_object to disk in GCT format
 #' 
@@ -138,12 +143,10 @@ to_GCT <- function(mat, cdesc=NULL, rdesc=NULL, version = NULL) {
 #' @family GCTX parsing functions
 #' @export
 
-write_gct <- function(ds, ofile, precision=4, appenddim=F, ver=3) {
+write_gct <- function(ds, ofile, precision=4, ver=3) {
   if (!class(ds)=="GCT_object") {
     stop("ds must be a GCT_object")
   }
-  # append the dimensions of the data set, if desired
-  if (appenddim) ofile <- append.dim(ofile, ds@mat, extension="gct")
   
   precision = floor(precision)
   cat(sprintf('Saving file to %s\n',ofile))
@@ -213,4 +216,189 @@ write_gct <- function(ds, ofile, precision=4, appenddim=F, ver=3) {
   cat(sprintf('Saved.\n'))  
 }
 
+
+#### define helper method for parsing gct files ###
+
+#' Adjust the data types for columns of a meta data frame
+#' 
+#' @description GCT(X) parsing initially returns data frames
+#'   of row and column descriptors where all columns are of
+#'   type character. This is inconvenient for analysis, so
+#'   the goal of this function is to try and guess the
+#'   appropriate data type for each column.
+#'   
+#' @param meta a data.frame
+#' 
+#' @details This is a low-level helper function
+#'   which most users will not need to access directly
+#' 
+#' @return meta the same data frame with (potentially) adjusted
+#'   column types.
+#'   
+#' @examples
+#' \dontrun{
+#' str(cdesc_char)
+#' fixed <- mapGCT:::fix.datatypes(cdesc_char)
+#' str(fixed)
+#' }
+#' 
+#' @keywords internal
+fix.datatypes <- function(meta) {
+  for (field.name in names(meta)) {
+    # get the field values
+    field <- meta[[field.name]]
+    field.except.na <- field[!is.na(field)]
+    # check if it's numeric. data may come in as a string
+    # but actually contains numeric values. if so, as.numeric
+    # will not result in a vector of NA values
+    field.as.numeric <- suppressWarnings(as.numeric(field.except.na))
+    if (!any(is.na(field.as.numeric))) {
+      field <- as.numeric(field)
+    }
+    if (is.numeric(field)) {
+      # check if it's an integer. data may be floats but
+      # if we coerce to an integer and the difference from
+      # original values is zero, that means data are actually
+      # integers. integer conversion will return NA if there
+      # are any issues.
+      field.except.na <- field[!is.na(field)]
+      field.as.integer <- suppressWarnings(as.integer(field.except.na))
+      if (!any(is.na(field.as.integer))) {
+        # integer conversion was fine, lets see if the
+        # values are altered
+        diffs <- field.except.na - field.as.integer
+        if (all(diffs == 0)) {
+          # converting to integer didn't change value,
+          # set field to integer values
+          field <- as.integer(field)
+        }
+      }
+    }
+    # insert back into the annotations
+    meta[[field.name]] <- field
+  }
+  return(meta)
+}
+
+
+#' Parse a GCT file into the workspace as a GCT_object
+#' 
+#' @param fname path to the GCT file on disk
+#' @param set_annot_rownames boolean indicating whether to set the
+#'   rownames on the row/column metadata data.frames. Set this to 
+#'   false if the GCT file has duplicate row/column ids.
+#' @param matrix_only boolean indicating whether to parse only
+#'   the matrix (ignoring row and column annotations)
+#'
+#' @details \code{parse_gct} also supports parsing of plain text
+#'   GCT files, so this function can be used as a general GCT parser.
+#' 
+#' @example
+#' \dontrun{
+#' (ds <- parse_gct("path/to/gct/file"))
+#' 
+#' # matrix only
+#' (ds <- parse_gct("path/to/gct/file", matrix_only=TRUE))
+#' }
+#' 
+#' @family GCT parsing functions
+#' @export
+parse_gct <- function(fname, set_annot_rownames=F, matrix_only=F) {
+  ds <- new("GCT_object", src = fname)
+  
+  if (!(grepl(".gct$", fname)))
+    stop("A .gct file must be given")
+  # get the .gct version by reading first line
+  ver = scan(fname, what = "", nlines = 1, sep = "\t", quiet = TRUE)[1]
+  # get matrix dimensions by reading second line
+  dimensions = scan(fname, what = double(0), nlines = 1, skip = 1, sep = "\t", quiet = TRUE)
+  nrmat = dimensions[1]
+  ncmat = dimensions[2]
+  if (length(dimensions)==4) {
+    # a #1.3 file
+    message("parsing as GCT v1.3")
+    nrhd <- dimensions[3]
+    nchd <- dimensions[4]
+  }else{
+    # a #1.2 file
+    message("parsing as GCT v1.2")
+    nrhd <- 0
+    nchd <- 0
+  }
+  message(paste(fname, nrmat, "rows,", ncmat, "cols,", nrhd, "row descriptors,", nchd, "col descriptors"))
+  # read in header line
+  header = scan(fname, what = "", nlines = 1, skip = 2, sep = "\t", quote = NULL, quiet = TRUE)
+  # construct row header and column id's from the header line
+  if ( nrhd > 0 ) {
+    rhd <- header[2:(nrhd+1)]
+    cid <- header[-(nrhd+1):-1]
+    col_offset <- 1
+  }else {
+    if (any(grepl("description", header, ignore.case=T))) {
+      # check for presence of description column in v1.2 files
+      col_offset <- 2
+    } else {
+      col_offset <- col_offset <- 1
+    }
+    rhd = NULL
+    cid = header[(1+col_offset):length(header)]
+  }
+  # read in the next set of headers (column annotations) and shape into a matrix
+  if ( nchd > 0 ) {
+    header = scan(fname, what = "", nlines = nchd, skip = 3, sep = "\t", 
+                  quote = NULL, quiet = TRUE)		
+    header = matrix(header, nrow = nchd, 
+                    ncol = ncmat + nrhd + 1, byrow = TRUE)
+    # extract the column header and column descriptions
+    chd = header[,1]
+    cdesc = header[,-(nrhd+1):-1]
+    # need to transpose in the case where there's only one column annotation
+    if ( nchd == 1 )
+      cdesc = t(cdesc)
+  }else {
+    chd = NULL
+    cdesc = data.frame()
+  }
+  # read in the data matrix and row descriptions, shape into a matrix
+  mat = scan(fname, what = "", nlines = nrmat, 
+             skip = 3 + nchd, sep = "\t", quote = NULL, quiet = TRUE)
+  mat = matrix(mat, nrow = nrmat, ncol = ncmat + nrhd + col_offset, 
+               byrow = TRUE)
+  # message(paste(dim(mat), collapse="\t"))
+  # Extract the row id's row descriptions, and the data matrix
+  rid = mat[,1]
+  if ( nrhd > 0 ) {
+    # need as.matrix for the case where there's only one row annotation
+    rdesc = as.matrix(mat[,2:(nrhd + 1)])
+    mat = matrix(as.numeric(mat[,-(nrhd + 1):-1]), nrow = nrmat, ncol = ncmat)
+  }else {
+    rdesc = data.frame()
+    mat = matrix(as.numeric(mat[, (1+col_offset):ncol(mat)]), nrow = nrmat, ncol = ncmat)
+  }
+  # assign names to the data matrix and the row and column descriptions
+  # message(paste(dim(mat), collapse="\t"))
+  dimnames(mat) = list(rid, cid)
+  if ( nrhd > 0 ) {
+    dimnames(rdesc) = list(rid,rhd)
+    rdesc = as.data.frame(rdesc, stringsAsFactors = FALSE)
+  }
+  if ( nchd > 0 ) {
+    cdesc = t(cdesc)
+    dimnames(cdesc) = list(cid,chd)
+    cdesc = as.data.frame(cdesc, stringsAsFactors = FALSE)
+  }
+  # assign to the GCT slots
+  ds@mat = mat
+  ds@rid = rownames(mat)
+  ds@cid = colnames(mat)
+  if (!matrix_only) {
+    # return annotations as well as matrix
+    ds@rdesc = fix.datatypes(rdesc)
+    ds@cdesc = fix.datatypes(cdesc)
+    # add id columns to rdesc and cdesc
+    ds@rdesc$id <- rownames(ds@rdesc)
+    ds@cdesc$id <- rownames(ds@cdesc)
+  }
+  return(ds)
+}
 
