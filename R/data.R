a61b06ef31698cb80674af992a931ccfa5b26770
Navin Chandan <navin.chandan@elucidata.io>
2018-01-07 11:51:21 +0530

    Added file data.R describing mapGCT datasets

---
+++ R/data.R
@@ -0,0 +1,20 @@
+# descriptions of mapGCT datasets
+
+#' An example table of column descriptions, as would be
+#' parsed from or parse_gct. Initially all the
+#' columns are of type character.
+"col_desc"
+
+#' An example table of row descriptons, as would be
+#' parsed from parse_gct or used to create gct_object
+#' by to_GCT
+"row_desc"
+
+#' An example of numeric matrix, as would be
+#' parsed from parse_gct or used to create gct_object
+#' by to_GCT
+"gct_mat"
+
+#' An example table of column annotations, which is to be 
+#' annotated to existing gct_object.
+"col_annotate"
